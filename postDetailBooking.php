0ba521cce4a9435f10c32275c9ee9c766aeda21e
lubisauliyak <lubis.au354@gmail.com>
2023-10-05 06:25:48 +0700

    Menambah comment

--- postDetailBooking.php
+++ postDetailBooking.php
@@ -2,19 +2,25 @@
 require_once '../../koneksi.php';
 
 if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+    // Ambil nilai parameter post[aksi yg dilakukan] pada body
     $action = test_input($_POST['action']);
+
+    // Menyesuaikan aksi dan mengambil nilai parameter post lainnya
     if ($action == 'detailBooking') {
+        // Cek apakah data QR Code kosong
         if (isset($_POST['kodeQr'])) {
             $kodeQr = test_input($_POST['kodeQr']);
 
+            // Mengambil data tiket_booking
             $sql = "SELECT * FROM akun_user au, tiket_booking tb WHERE tb.kode_qr = '$kodeQr' AND tb.id_user = au.id_user";
-
             $result = mysqli_query($conn, $sql);
             $count = mysqli_num_rows($result);
 
+            // Cek apakah ada data QR Code
             if ($count == 1) {
                 $data = mysqli_fetch_array($result);
 
+                // Tambahkan data tiket_booking ke response json
                 $response['text_message'] = 'Data booking tersedia.';
                 $response['id_booking'] = $data['id_booking'];
                 $response['id_user'] = $data['id_user'];
@@ -28,16 +34,23 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
             } else {
                 $response['text_message'] = 'Data booking tidak tersedia.';
             }
-        } else {
+        }
+        // Terdapat nilai parameter post yg masih kosong
+        else {
             $response['text_message'] = 'Data masukan belum ada.';
         }
-    } else {
+    }
+    // Terdapat nilai parameter post yg masih kosong
+    else {
         $response['text_message'] = 'Data masukan belum ada.';
     }
+    // Membuat response dalam format json
     echo json_encode($response);
-} else {
+}
+// Terdapat kesalahan pada request menggunakan HTTP
+else {
     $response['text_message'] = 'Kesalahan sistem HTTP.';
     echo json_encode($response);
 }
-
+// Menutup koneksi kedalam database
 $conn->close();
--- postTiketMasuk.php
+++ postTiketMasuk.php
@@ -2,7 +2,10 @@
 require_once '../../koneksi.php';
 
 if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+    // Ambil nilai parameter post[aksi yg dilakukan] pada body
     $action = test_input($_POST['action']);
+
+    // Menyesuaikan aksi dan mengambil nilai parameter post lainnya
     if ($action == 'tiketMasuk' && isset($_POST['idBooking']) && isset($_POST['idAdmin'])) {
         $idBooking = test_input($_POST['idBooking']);
         $idAdmin = test_input($_POST['idAdmin']);
@@ -10,7 +13,6 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
             $response['text_message'] = 'Kesalahan sistem, ulangi proses scan QR Code.';
         } else {
             $sql = "SELECT * FROM tiket_booking WHERE id_booking = '$idBooking'";
-
             $result = mysqli_query($conn, $sql);
             $count = mysqli_num_rows($result);
 
@@ -26,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
                 $insert = "INSERT INTO tiket_masuk (id_user, id_admin, tanggal_masuk, jumlah_tiket, total_bayar, metode_bayar) VALUES ('$idUser', '$idAdmin', '$tanggalMasuk', '$jumlahTiket', '$totalBayar', '$metodeBayar')";
                 $query = mysqli_query($conn, $insert);
                 if ($query) {
-                    // Menghapus data lama yang berada di database tiket Booking
+                    // JANGAN DIHAPUS : Menghapus data lama yang berada di database tiket Booking
                     // $delete = "DELETE FROM tiket_booking WHERE id_booking = '$idBooking'";
                     // $query = mysqli_query($conn, $delete);
                     $response['text_message'] = 'Tiket diterima.';
@@ -34,16 +36,21 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
                     $response['text_message'] = 'Tiket gagal diterima.';
                 }
             } else {
-                $response['text_message'] = 'Kesalahan sistem, coba lagi.';
+                $response['text_message'] = 'Kesalahan sistem, data QR Code tidak ditemukan.';
             }
         }
-    } else {
+    }
+    // Terdapat nilai parameter post yg masih kosong
+    else {
         $response['text_message'] = 'Kesalahan sistem, ulangi proses scan QR Code.';
     }
+    // Membuat response dalam format json
     echo json_encode($response);
-} else {
+}
+// Terdapat kesalahan pada request menggunakan HTTP
+else {
     $response['text_message'] = 'Kesalahan Sistem HTTP.';
     echo json_encode($response);
 }
-
+// Menutup koneksi kedalam database
 $conn->close();
