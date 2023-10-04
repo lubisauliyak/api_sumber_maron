edbcb42a976054ff0586e85996cd371a960455d7
lubisauliyak <lubis.au354@gmail.com>
2023-10-05 06:25:27 +0700

    Ganti MD5 dengan SHA256

--- postLogin.php
+++ postLogin.php
@@ -1,39 +1,52 @@
 <?php
-// require_once '/home/u625466052/domains/llubisa.tech/koneksi.php';
 require_once '../../koneksi.php';
 
 if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+    // Ambil nilai parameter post[aksi yg dilakukan] pada body
     $action = test_input($_POST['action']);
+
+    // Menyesuaikan aksi dan mengambil nilai parameter post lainnya
     if ($action == 'login') {
+        // Cek apakah ada data yg masih kosong
         if (isset($_POST['email']) && isset($_POST['password'])) {
             $email = test_input($_POST['email']);
             $password = test_input($_POST['password']);
-            $password = md5($password);
+            // Hash password menggunakan SHA256
+            $password = hash('sha256', 'SM-' . $password);
 
+            // Mengambil data akun_user
             $sql = "SELECT * FROM akun_user WHERE email = '$email' AND password = '$password'";
-
             $result = mysqli_query($conn, $sql);
             $count = mysqli_num_rows($result);
 
+            // Cek apakah ada data user
             if ($count == 1) {
                 $data = mysqli_fetch_array($result);
 
+                // Tambahkan data akun_user ke response json
                 $response['text_message'] = 'Login berhasil.';
                 $response['id_user'] = $data['id_user'];
                 $response['name_user'] = $data['nama'];
             } else {
                 $response['text_message'] = 'Login gagal.';
             }
-        } else {
+        }
+        // Terdapat nilai parameter post yg masih kosong
+        else {
             $response['text_message'] = 'Data masukan kurang lengkap.';
         }
-    } else {
+    }
+    // Terdapat nilai parameter post yg masih kosong 
+    else {
         $response['text_message'] = 'Data masukan kurang lengkap.';
     }
+    // Membuat response dalam format json
     echo json_encode($response);
-} else {
+}
+// Terdapat kesalahan pada request menggunakan HTTP
+else {
     $response['text_message'] = 'Kesalahan sistem HTTP.';
     echo json_encode($response);
 }
-
+// Menutup koneksi kedalam database
 $conn->close();
